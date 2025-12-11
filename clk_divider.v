f9319fc0cefdb8af41d3272a30934feb74f093e0
Wag327 <wag327@lehigh.edu>
2025-12-11 13:34:25 -0500

    Remove redundant comments and clean up code

--- clk_divider.v
+++ clk_divider.v
@@ -34,14 +34,3 @@ always @ (posedge clock_in) begin
     end
 end
 endmodule
--------------------------------------------------------------------------------------------
-module clk_divider_tb;
-    reg clock_in;
-    wire clock_out;
-    
-    clk_divider uut(.clock_in(clock_in), .clock_out(clock_out));
-    initial begin
-        clock_in = 0;
-        forever #10 clock_in = ~clock_in;
-    end
-endmodule
